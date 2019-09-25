2180dbfc330390e2a50658e0cb899ac31721012b
jacobboykin <boykinmusic@gmail.com>
2019-09-25 11:29:54 -0700

    add finished hello

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -11,15 +11,27 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jacob")
-		want := "Hello, Jacob"
+	t.Run("to a person", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("empty string defaults to 'World'", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, world"
+	t.Run("empty string", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", spanish)
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Lauren", french)
+		want := "Bonjour, Lauren"
 		assertCorrectMessage(t, got, want)
 	})
 
